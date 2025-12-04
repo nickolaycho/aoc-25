32f50b9cbc8f0c68b87b2ceafafa3f50295d6d9f
NicolaOrecchini <nicola.orecchini@agilelab.it>
2025-12-04 11:40:58 +0100

    day 4 part 2

--- Day04/day04.py
+++ Day04/day04.py
@@ -11,10 +11,10 @@ class Grid():
         self.input_file = input_file
         self.columns = len(self.input_file[0])
         self.rows = len(self.input_file)
+        self.padded_grid = self.pad_grid()
+        self.rolls_of_paper = self.count_total_rolls_of_paper()
 
-
-    @cached_property
-    def padded_grid(self) -> list[str]:
+    def pad_grid(self) -> list[str]:
         padded_grid: list[str] = self.input_file.copy()
         horizontal_pad: str = "." * (self.columns)
         padded_grid.insert(0, horizontal_pad)
@@ -23,7 +23,7 @@ class Grid():
 
         return padded_grid
 
-    def count_accessible_cells(self) -> int:
+    def count_accessible_rolls_of_paper(self) -> int:
         num_accessible_cells: int = 0
         for row in range(1, len(self.padded_grid)):
             for col in range(1, len(self.padded_grid[0])):
@@ -32,6 +32,43 @@ class Grid():
                     num_accessible_cells += 1
 
         return num_accessible_cells
+
+    def mark_cell_as_removed(self, row: int, col: int) -> None:
+        self.padded_grid[row] = self.padded_grid[row][:col] + "x" + self.padded_grid[row][col+1:]
+
+    def count_total_rolls_of_paper(self):
+        tot_rolls_of_paper: int = 0
+        for row in range(1, len(self.padded_grid)):
+            for col in range(1, len(self.padded_grid[0])):
+                cell = Cell(self.padded_grid, row, col)
+                if cell.is_roll_of_paper:
+                    tot_rolls_of_paper += 1
+        return tot_rolls_of_paper
+
+    def remove_accessible_rolls_of_paper(self):
+        removed_rolls_of_paper: int = 0
+        for row in range(1, len(self.padded_grid)):
+            for col in range(1, len(self.padded_grid[0])):
+                cell = Cell(self.padded_grid, row, col)
+                if cell.is_accessible_by_forklift():
+                    self.mark_cell_as_removed(row, col)
+                    removed_rolls_of_paper += 1
+
+        return removed_rolls_of_paper
+
+    def remove_rolls_in_cycles(self):
+        cumulative_rolls_removed: int = 0
+        while self.rolls_of_paper > 0:
+            removed_rolls = self.remove_accessible_rolls_of_paper()
+            if removed_rolls == 0:
+                break
+            self.rolls_of_paper -= removed_rolls
+            cumulative_rolls_removed += removed_rolls
+
+            print(f"Removed in cycle: {removed_rolls}; Cumulative removed: {cumulative_rolls_removed}; Remaining {self.rolls_of_paper} rolls")
+
+
+
 
 
 class Cell():
@@ -42,6 +79,7 @@ class Cell():
         self.grid = grid
         self.row_coordinate = x
         self.column_coordinate = y
+        self.cell_value = self.grid[self.row_coordinate][self.column_coordinate]
 
     @cached_property
     def adjacent_cells(self) -> list["Cell"]:
@@ -57,7 +95,7 @@ class Cell():
 
     @cached_property
     def is_roll_of_paper(self) -> bool:
-        return self.grid[self.row_coordinate][self.column_coordinate] == "@"
+        return self.cell_value == "@"
 
     @cached_property
     def rolls_of_paper_in_adjacent_cells(self) -> int:
--- Day04/main.py
+++ Day04/main.py
@@ -6,8 +6,11 @@ def main():
     input_file = get_input(input_path)
 
     grid = Grid(input_file=input_file)
-    result = grid.count_accessible_cells()
+    result = grid.count_accessible_rolls_of_paper()
     print("Part 1 result:", result)
+
+    print("Part 2 result:")
+    grid.remove_rolls_in_cycles()
 
 if __name__ == "__main__":
     main()
