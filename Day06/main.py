7a30e4592f9ec32c458a96f70f441e569ff65e33
niko <nicolaorecchini@gmail.com>
2025-12-14 16:34:49 +0100

    rename

---
+++ Day06/main.py
@@ -0,0 +1,26 @@
+from dayXX import *
+import time
+
+def main():
+
+    input_path = "input.txt"
+    input_file = get_input(input_path)
+
+    grid = Grid(input_file=input_file)
+
+    start = time.perf_counter()
+    part_1_result = grid.largest_possible_rectangles_area
+    end = time.perf_counter()
+    print("Part 1 result: ", part_1_result)
+    print(f"Part 1 time: {(end - start):.3f} s")
+
+    print("Part 2")
+    start = time.perf_counter()
+    part2_result = grid.largest_area_constrained
+    end = time.perf_counter()
+    print("Part 2 result: ", part2_result)
+    print(f"Part 2 time: {(end - start):.3f} s")
+
+
+if __name__ == "__main__":
+    main()
